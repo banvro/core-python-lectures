a1ed043a2c4d07a44bedbbae5b8379ec13115b1a
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-06-30 18:02:19 +0530

    Update variables_data_tyoes_core_fun.py

---
+++ python-for-online-random/variables_data_tyoes_core_fun.py
@@ -0,0 +1,267 @@
+
+# Variables : 
+#     are contaieners, help us to store data, so we can use that data in future
+
+# a = 100
+
+# a = 1000
+
+# b = 500
+
+# c = a + b
+
+# print(c)
+
+
+
+
+# Rules to create variable:
+
+# 1) always start from charcters or _.
+
+# age = 30
+
+# a2 = 50
+
+# _2 = 50
+
+
+# 2) Never be start from integers
+
+
+# 1 = 300
+# 2a = 400
+
+# 3) Never help spaces
+
+
+
+# my_name = "Naresh"
+
+
+# 4) Never hold spacial symbols
+
+# !@#$%^&*()(*)_)*9097
+
+# a@ = 40
+
+
+# 5) Never be a keyword.
+
+
+# keyword: reserved words
+
+# if, for, def, class, break, continue, pass, return
+# 32
+
+
+# if = 200
+
+
+# where_if
+
+# if_ = 3000
+
+
+# _ = 20
+
+
+
+
+# Comments:
+#     help to increse code readbility
+
+# A
+# B
+# C
+
+
+# addding two numbers
+# a = 10
+# b = 20 # string values
+
+# c = a + b # adding numbers
+
+# # printing data...
+# print(c)
+
+
+
+# Types of comments
+    # 1) Single line comment
+    
+    # tbkjabdakjbda
+    
+    
+    # 2) Multi line comment
+
+'''
+kjbkjada
+sdjkabsdkasd
+ajsbdkasd
+asdbaksdbasd
+ajsbdaskbd
+'''
+
+
+
+# akjsbkdas
+# asbjdkabsd
+# asjdbkasd
+# # asjbdkad
+# askjbdkasd
+
+
+# -------------------------------------
+
+# Data Types:
+#     ---> 
+#     1) Integer 
+#         10, 34, -100, 0, 3434, -1231
+        
+#     2) Float
+#         10.0, 45.1, 1.0, 34.56
+    
+#     3) String
+#         'this is a string'
+#         "this is also a string"
+#         """this is also a string"""
+#         '''this is also a string'''
+   
+#     4) Boolean
+#         True / False
+        
+#     5) Complex:
+#         a + bj
+#         10 + 20j
+      
+#      Data Structures of Pyhton   
+#     6) List
+#     7) Tuple
+#     8) Set
+#     9) Dictionry
+    
+
+    
+# Core Functions
+
+# function
+#     --->
+    
+# x()
+
+# xyz()
+
+# hello()
+
+
+# 1) Built In functions
+# 2) User define functions
+    
+
+# 1) print()
+#     : help us to show anything on console.
+# help you to print single line
+# print("Hellow world")
+
+# print('hey! how are you')
+
+# Multi line string
+
+# print("""heyyyyyyyyyy""")
+    
+    
+    
+# print("name ---> naresh age ---> 25")
+    
+
+
+# print("""name ---> nareshhhh
+# age ----- 50
+# number ---- 8219836118""")
+
+
+
+# x = 1000
+
+# y = 500
+
+# print(x, y, "hello", x, y, y, y, y)
+
+
+# a = 10
+
+# b = 20
+
+# c = ?
+
+# the sum of 10 and 20 is 30
+
+# name = "naresh"
+# age = 25
+# number = 8219836118
+# email = "programography@gmail.com"
+
+# print("Your name is", name, "and your age is", age, "and the phone number is", number, "and email id is", email)
+
+
+# f-string
+
+# print(f"Your name is {name} and age is {age} and phone number is {number} and email id is {email}")
+
+
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(f"the sum of {a} and {b} is : {c}")
+
+
+
+# a = 10
+# b = 20
+# c = 56
+
+# print(a, b, c, "hello", a, sep = ", ")
+
+
+
+
+# a = 10
+# b = 20
+# c = 56
+
+# print(a, end = "\n")
+# print(b)
+# print(c)
+
+
+
+# print("helooo!,\n heyyy how \nwhat")
+
+
+
+# type()
+# ----> what type of data a variable hold.
+
+
+# x = 10 + 56j
+
+# print(type(x))
+
+
+
+
+
+# print(True + True + False)
+
+
+print(type(6 / 2))
+
+
+
+
+
+
