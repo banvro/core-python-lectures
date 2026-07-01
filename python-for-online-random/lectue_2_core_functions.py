085ed8032d6b6c3c8709e87a621849224d3e3c18
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-01 11:02:21 +0530

    Create lectue_2_core_functions.py

---
+++ python-for-online-random/lectue_2_core_functions.py
@@ -0,0 +1,291 @@
+# Variables
+# Data Types
+# Core functions:
+#         --> print()
+#         --> type()
+
+
+
+# x = 1000
+
+
+# my_name = "naresh"
+
+# age = 25
+
+# phone_numbe = 8219836118
+
+
+
+# a = 100
+
+# b = 400
+
+# c = a + b 
+
+# print(c)
+
+
+
+
+
+# Integer
+#     10, 34, 45, -100, 34, 54, -566
+
+# 2) Float
+#     10.0, 4.5, 23.6
+
+# 3) String
+#     'this is a string'
+#     "this is also a string"
+#     """this is also a string"""
+#     ''''this is also a string'''
+    
+#     "10"
+
+# 4) Boolan:
+#     True / False
+
+# 5) Complex
+
+# 6) List
+# 7) Tuple
+# 8) Set
+# 9) Dictionry
+
+
+
+
+
+
+# print()
+
+# a = 10
+
+# b = 20
+
+# c = a + b
+
+# print(a, b, c, a, a, a)
+
+# ok = 10
+
+# print('hello world')
+
+# print("heyyyy")
+
+# print(ok)
+
+
+
+# a = 2
+
+# b = 4
+
+# c = a + b
+
+# print("The sum of", a, "and", b, "is :", c)
+
+
+# input()
+#     --> help us to get input from user.
+
+
+# program : adding two numbers
+
+# a = 50
+
+# b = 60
+
+# c = a + b
+
+# print(c)
+
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+
+
+# print("User name is : ", name, "and age is ", age)
+
+
+
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# zx = num1 + num2
+
+# print(zx)
+
+
+
+# input()
+#     ---> string
+
+
+# print("90" + "100")
+
+# Concatenation : Joining Two strings
+
+
+# Type casting:
+#     to convert data from one type to another
+    
+    
+# zx = 10
+
+# print(zx, type(zx))
+
+# q = float(zx)
+
+# print(q, type(q))
+    
+    
+    
+#   integer ---> int
+   
+#   float ---> float
+   
+#   string ---> str
+   
+#   boolen ---> bool
+   
+#   list --> list
+   
+#   tuple ---> tuple
+#   set -----> set
+#   dictionry ----> dict
+    
+    
+
+# zx = "1000"
+
+# print(zx, type(zx))
+    
+# qw = int(zx)
+
+# print(qw, type(qw))
+
+
+
+
+
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# x = int(num1)
+# y = int(num2)
+
+# zx = x + y
+
+# print(zx)
+
+
+
+
+
+
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter second number : "))
+
+# zx = num1 + num2
+
+# print(zx)
+
+
+# zx = "this"
+
+# print(zx, type(zx))
+
+# x = int(zx)
+
+# print(x, type(x))
+
+
+
+
+
+# a = 10
+
+# b = "20"
+
+# print(a + b)
+
+
+
+# print("heloooo" * 10)
+
+
+
+
+# Objects:
+#     1) Ittrable objects
+#         string, list, tuple, set, dicitony
+    
+#     2) Non Ittrable Objects
+#         integer, float, boolean , complex
+
+
+# len(), count(), loops, indexing, slicing..
+
+# ...........> ittrable objects
+
+
+# len()
+#     ---> help to know about the total number of elements in a ittrable objects
+
+
+
+# zx = "this is a car and a good car."
+
+# print(len(zx))
+
+
+
+# count()
+#     ---> help to know about the occurence of a element...
+
+
+# zx = "this is a car and this is a good car"
+
+# syntax
+
+# variable_name.count("----")
+
+# print(zx.count("is"))
+
+
+
+
+
+# Variables
+# Data Types
+# Comments
+# Type Casting
+# Core functios:
+#     print()
+#     input()
+#     len()
+#     count()
+#     type()
+
+
+
+
+
+zx = 1000200300003000000004
+
+zx = str(zx)
+
+print(zx.count("0"))
+
+
+
+# fucntion()
+
+
+# oops 
+
