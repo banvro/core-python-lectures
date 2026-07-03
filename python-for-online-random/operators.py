c771a162195d03a6eebb7a09b18504731ec31aae
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-03 10:28:23 +0530

    Create operators.py

---
+++ python-for-online-random/operators.py
@@ -0,0 +1,245 @@
+# Core functions
+
+# count()
+    # --> help us to get the ocurence of an element.
+    
+# zx = "this is a car and this is a good car"
+
+# print(zx.count("is"))
+
+# string | list | tuple | set | dicitionry
+
+
+
+
+
+
+# zx = 100003000300030300000230000230
+
+# zx = str(zx)
+
+
+# print(zx.count("0"))
+
+
+
+# print()
+# input()
+# len()
+# count()
+# type()
+
+
+
+
+
+# Operators:
+    
+
+# 10 + 20
+
+# 10, 20 ----> operends
+# + -----> operator
+
+# 1) Arithmetic operators
+# 1) + 
+# 2) -
+# 3) /
+# 4) *
+# 5) % ----> Modules
+# 6) // ---> floor devision
+# 7) ** ----> exponential
+
+# print(10 + 20)
+
+# concatenation
+
+# print("10" + 20)
+
+# print(10 - 3)
+
+
+
+# devsion: ----> float
+
+# print(10 / 2)
+
+# print(3 / 2)
+
+
+# floor devsion
+# print(3 // 2)
+
+
+# print(12 * 3)
+
+# print("5" * 60)
+
+
+# print(2 * 3)
+
+# print(2 ** 3)
+
+# print(4 ** 5)
+
+# print(5 % 2)
+
+# print(23 % 9)
+
+
+
+
+# 2) Compersion Operators:
+#     --> True | False
+    
+    # 1) ==  ---> equal to
+    # 2) != ---> not equal to
+    # 3) >
+    # 4) <
+    # 5) >=
+    # 6) <=
+
+
+# print(10 == 9)
+
+# print("hello" == "hello")
+
+# print(10 != 8)
+
+# print(10 > 40)
+
+# print(10 > 10)
+
+# print(100 <= 100)
+
+
+
+
+# 3) Logical Operators
+    # --> True | False
+    
+    # 1) and
+    # 2) or
+    # 3) not
+
+
+# age = 20
+# cu = "USA"
+
+
+# print(age > 18 and cu == "India")
+
+
+# or
+
+# print(age > 18 or cu == "India")
+
+# not
+
+# print(not(10 != 10))
+
+
+# Bitwise operator
+
+
+# 1) &
+
+# print(10 & 4)
+
+# print(45 & 8)
+
+# 2) |
+
+# print(34 | 8)
+
+
+# 3) ~
+
+# -(x + 1)
+
+# print(~(100))
+
+
+
+
+
+# Mambership Operators
+    # --> True | False
+    
+    # 1) in
+    # 2) not in
+
+
+# zx = "this is  a car"
+
+# q = "car"
+
+# print(q not in zx)
+
+
+
+# Identity Operatos
+
+# is
+# not is
+
+# a = 100
+# b = 23
+
+# print(a is b)
+
+# print(a is not b)
+
+
+
+
+
+# Assignment Operators
+
+
+# =
+
+# a = 10
+
+# b = a
+
+# print(b)
+
+
+# +=
+# -=
+
+
+# a = 10
+
+
+# a = a + 2
+# a -= 3 # a = a - 3
+
+# print(a)
+
+# +=
+# -=
+# /=
+# *=
+# %=
+# //=
+# **=
+
+
+
+x = 3
+
+x **= 3
+
+# x = x ** 3
+
+print(x)
+
+
+
+
+
+
+
+
