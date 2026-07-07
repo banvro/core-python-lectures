6d44277bf8badd546075210abecc5a10b69a564f
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-07 13:29:55 +0530

    Create for_loop.py

---
+++ python-for-online-random/for_loop.py
@@ -0,0 +1,144 @@
+# Loops:
+    
+#     # help us to do ittrations or repetaions
+    
+# Objects:
+    
+# Loop ---> ittrable objects
+
+# zx = "this is a car"
+
+
+# syntax
+
+# for i in seq:
+    # asdas
+
+
+# zx = "this is a car"
+
+# for i in zx:
+#     print(i)
+#     print("hellooooo")
+#     print("ooookk", i, i, i, i)
+#     print("done...")
+
+# print("ppppppppppp")
+# print("qqqqqqqqqqqq")
+
+
+
+# vr = "123412"
+
+# for j in vr:
+#     print("j", j)
+    
+# ittrable objects
+
+# range() ---> data genrate
+
+
+# for t in range(1, 20, 1):
+#     print(t)
+
+
+# for i in range(3, 15, 2):
+#     print(i)
+
+
+# for j in range(10, 0, -1):
+#     print(j)
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# .
+# .
+# .
+# .
+# 2 x 10 = 20
+
+# for i in range(1, 11):
+#     print("2 x", i, "=", 2*i)
+
+
+# fizzfuzz
+
+# 1 --- 50
+
+# 3 ---> fizz
+# 5 ---> buzz
+# 3 & 5 ---> fizzbuzz
+
+# 1
+# 2
+# fizz
+# 4
+# buzz
+# fizz
+# 7
+# 9
+# fizz
+# buzz
+# 11
+# fizz
+# 13
+# 14
+# fizzbuzz
+# 16
+# 17
+
+
+
+# for i in range(1, 51):
+    
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("fizzbuzz")
+    
+#     elif i % 5 == 0:
+#         print("buzz")
+    
+#     elif i % 3 == 0:
+#         print("fizz")
+    
+#     else:
+#         print(i)
+
+
+
+
+# zx = [12, 34, 45, 56, 5, 6]
+
+# for i in zx:
+#     print(i)
+
+
+
+
+zx = [("Bus 1", 23, 45), ("Bus 2", 34, 50), ("Bus 3", 1, 4)]
+
+
+for i in zx:
+    
+    print(f'''
+    My Bus name is : {i[0]}
+    Org : {i[1]} old price : {i[2]}
+    ''')
+    
+    print("Add to cart")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+    
