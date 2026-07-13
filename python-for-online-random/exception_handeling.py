d322f1b7e1e09c8e71af99c30d015499eee6b41e
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-13 11:36:18 +0530

    Create exception_handeling.py

---
+++ python-for-online-random/exception_handeling.py
@@ -0,0 +1,137 @@
+
+# Exception handeling:
+
+    # Errors:
+    #     1) Syntax errors
+    #     2) Logical Error
+    #     3) Run Time error
+    
+
+# 1) Syntax Error:
+    
+
+
+# age = 18
+
+# if age == 18
+#     print("eligible for vaot..!")
+
+
+
+
+
+
+
+# a  10
+
+# b = 20
+
+
+
+# 2) Logical Error:
+
+
+# a = 10
+
+# b = 0
+
+# c = a / b
+
+# print(c)
+
+
+
+# 3) Run Time error:
+
+
+# age = int(input("Enter your age : "))
+
+# print(age)
+
+
+
+# a = 10
+
+# b = 20
+
+# c = 89
+
+# z = a + b
+
+# print("hellliiii")
+
+# print("heyyyyyyyyyyyy!")
+
+# print(gooo)
+
+# zx = b + c
+
+# print("doneee")
+
+# print("okkkkkk")
+
+
+# Exception Handleing:
+
+# blocks
+
+# try:
+#     # doutable code
+    
+# except Exception:
+#     # execute if try block having error
+
+# else:
+#     # code--> if try block have't any error
+
+# finally:
+#     # it just indecate exception handeling is done
+
+
+# print("my code start...")
+
+# print("hellooooo")
+
+# try:
+#     age = int(input("Enter your age : "))
+
+# except Exception:
+#     print("Error occuer! handeled")
+
+# else:
+#     print("User age is :", age)
+
+# finally:
+#     print('Exception hadnling done.!')
+
+# print("movingggg on")
+
+# print("goooooo")
+
+# print("done...!")
+
+
+
+print("heyyyyyyyyyyy")
+print("nooooooooooooooooooooo")
+print("gooooooooo")
+
+a = 10
+b = 0
+
+# print(a / b)
+try:
+    zx = a / b
+    print(zx)
+
+except Exception as e:
+    print(e)
+    
+print("mvig on")
+print("ok")
+
+
+
+
+
+
