8d3c756d3a3562d6f2d189e94d908ea1dd2005c5
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-14 19:29:58 +0530

    Create operators_new.py

---
+++ python-for-online-random/operators_new.py
@@ -0,0 +1,290 @@
+# Veriables
+# Data Types
+# print()
+# input()
+# type()
+# Type casting
+# comments
+
+
+# Comments:
+#     to insrease code readablitly
+    
+
+# pro
+
+# a
+# b
+# c  --> 500
+
+# types of comments:
+#     single line comment
+    
+#     #    kajsdbkjabdkja
+    
+#     multi line comment
+    
+#     '''
+#     aksbda
+#     sdajkdsbkasdajsbdad
+#     asdkbads
+#     asb
+#     asdbakdb
+#     '''
+
+
+# len()
+#     ---> help us to know about the total number of elemtns in an ittrable object
+
+
+
+# Objects:
+#     1) ittrable objects
+#         string, list, tuple, set, dict
+    
+    
+#     2) Non-ittrable objects
+#         integer, float, boolean, complex
+
+# len(), count(), for loop, indexing, slicing, ....   ---> ittrable objects
+
+
+# len()
+
+# zx = "1467"
+
+# print(len(zx))
+
+
+# zx = "this is a car and this is a go od car"
+
+
+# print(len(zx))
+
+
+
+# count()
+#     ---> help us to know about the occurence of an element in an ittrable object.
+
+
+# zx = "10002000"
+
+# # syntax
+
+# # varibale_name.count("??")
+
+# print(zx.count("0"))
+
+
+# zx = "this is a car and this is a good car"
+
+# print(zx.count("is"))
+
+
+# zx = 102000200003304
+
+# zx = str(zx)
+
+# print(zx.count("0"))
+
+# print()
+# type()
+# input()
+# len()
+# count()
+
+
+# ------------------------------------------------
+
+# Operators
+
+# 10 + 20
+
+# 10, 20 ---> operends
+# + ---> operator
+
+
+# 1) addtion   +
+
+# print(10 + 20)
+
+# print("100" + "300")
+
+# print(1000  + "56666")
+
+
+# 2) subtraction
+
+# print(100 - 19)
+
+# 3) Multiplication
+
+# print(2 * 6)
+
+# print("hello " * 100)
+
+# 4) devision
+    # ----> float
+
+# print(6 / 3)
+
+# print(5 / 4)
+
+# 5) floor devsion
+
+# print(5 // 4)
+
+
+# print(22 // 11)
+
+
+
+# 6) ** --> exponential
+
+
+# print(2 ** 3)
+
+# print(4 ** 5)
+
+# 4 x 4 x 4 x 4 x 4 = 
+
+
+# 7) Modules  %
+
+
+
+# print(-3 % 2)
+
+# print(6 % 4)
+
+
+# 1) arithmetical Operators
+#     +
+#     -
+#     /
+#     //
+#     *
+#     **
+#     %
+    
+# 2) Compersion Operators
+#     ---> True / False
+
+    # == ---> equal to
+    
+# print(100 == 100)
+
+# print("hello" == "hello")
+
+# print(100 == "100")
+
+
+# 2) != ----> not equal to
+
+
+# print(10 != 5)
+
+# print(10 != 10)
+
+
+# 3) > ----> greater then
+
+
+
+# print(10 > 4)
+
+# print(100 > 100)
+
+
+# < -- less then
+
+# print(8 < 10)
+
+# print(100 < 45)
+
+
+# >= --- greater then equal to
+
+# print(100 >= 100)
+
+
+
+# ----> Logical Operators
+#     1) and
+#     2) or
+#     3) not
+
+# and
+# 0   0 | 0
+# 1   0 | 0
+# 0   1 | 0
+# 1   1 | 1
+
+
+
+# age = 20
+# country = "India"
+
+# print(age > 18 and country == "India")
+
+
+# or
+
+# 0   0  | 0
+# 1   0  | 1
+# 0   1  | 1
+# 1   1  | 1
+
+# age = 20
+# country = "India"
+
+# print(age > 21 or country == "USA")
+
+
+# not
+
+# print(not(10 == 10))
+
+
+# -----------------------------------------
+
+# Mambership Operators
+
+# 1) in
+# 2) not in
+
+# print("car" in "this is a car")
+
+# x = "this is a car"
+# y = "plan"
+
+# print(y in x)
+
+# print(y not in x)
+
+
+# 1) &
+
+# print(45 | 23)
+
+# print(~(45))
+
+
+
+# ------------------------------
+
+# identity Operator
+    # 1) is
+    # 2) not is
+
+
+a = 10
+b = 100
+
+print(a is not b)
+
+
+
+
+
+
