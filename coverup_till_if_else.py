ec66aa6f62cb3aff765bacd0c627200498ba3f3d
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-18 16:56:07 +0530

    Create coverup_till_if_else.py

---
+++ coverup_till_if_else.py
@@ -0,0 +1,346 @@
+
+# print("this is a car")
+
+# print("heloooo")
+
+# x = 10
+
+# w = 10000
+
+# print(w)
+
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(a, b, c, "hello", "no", c, c, c, sep = "- ")
+
+# print("the sum of", a, "and", b, "is :", c)
+
+
+# input()
+#     ---> help to get input from user
+
+
+
+# a = 10
+# b = 78
+
+# c = 12 + 90
+
+# print(c)
+
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+
+# print(name, age)
+
+# name
+# age
+# number
+# email
+# adress 
+
+
+# 2) addtion
+
+
+# print("name : naresh age : 25")
+
+# print("""name : naresh
+# age : 25
+# number : 8219835119""")
+
+
+
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# zx = num1 + num2
+
+# print("the sum of", num1, "and", num2, "is :", zx)
+
+
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print("the sum of", a, "and", b, "is :", c)
+
+# # f-string
+
+# print(f"the sum of {a} and {b} is : {c}")
+
+
+
+
+
+
+
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# zx = num1 + num2
+
+# print("the sum of", num1, "and", num2, "is :", zx)
+
+# concatenation 
+#     --< joining two strings
+
+
+# input()
+#     --> string
+
+# print("1000" + "4000")
+
+# print(1000 + 4000)
+
+
+# Type casting:
+#     --> chnage data type of a veribale
+
+
+# type()
+#     --> help us to know about the type of data a veribale hold
+
+
+# zx = 10 + 34j
+
+# print(type(zx))
+
+
+# a = 100
+
+# print(a, type(a))
+
+# b = float(a)
+
+# print(b, type(b))
+
+
+
+# zx = "4000"
+
+# x = int(zx)
+
+# print(x, type(x))
+
+# int ---> int()
+# float----> float()
+# sting --> str()
+# booen --> bool()
+
+
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter first number : "))
+
+
+# print(num1 + num2)
+
+# print()
+# input()
+# type()
+
+# len()
+
+
+# objects:
+#     1) ittrable objects
+#         string, list, tuple, set, dict
+    
+#     2) Non Ittrable objects
+#         integer, flaot, boolean, complex
+
+
+# len(), count(), indexing, slicinig, loops...... ittrable objects
+
+
+# len()
+#     --> help to know about total number of items in an ittrale object
+
+# zx = "12348"
+
+# print(len(zx))
+
+
+# x = 1001
+
+# x = str(x)
+
+# print(len(x))
+
+
+
+# ---> user ----> input()
+
+# xyz = done
+
+# print(done have length : 4)
+
+
+# zx = input("ENter your data : ")
+
+# print(zx, "have length :", len(zx))
+
+
+
+# count()
+#     --> help to know about the occurence of an element in an ittrable objects
+
+
+# zx = "this is a car and this is a good car"
+
+# syntax
+# veriable_name.count(???)
+
+# print(zx.count(" "))
+
+
+# a = 10
+# b = 20
+
+# print(a, b, a + b, b - a, "helloo", sep = "-")
+
+
+
+
+
+# a = 10
+# b = 30
+
+# print(f"the sum of {a} and {b} is : {a + b}")
+
+
+# age = input("ENter your age : ")
+
+# print("your age is : ", age)
+
+
+# dob_year = int(input("Enter your born year : "))
+
+# age = 2026 - dob_year
+
+# print("your age is : ", age)
+
+
+
+# type():
+    
+# zx = False
+
+# print(type(zx))
+
+
+# len()
+#     --
+
+# x = 129823
+
+# print(len(x))
+
+
+# count()
+
+# x = "helloo, how are you! heyy"
+
+# print(x.count("o"))
+
+
+# ---------------------
+
+# find the occureces of find in data
+
+# data = ?
+
+# find = ?
+
+
+# data = input("Enter your data : ")
+
+# find = input("What you find : ")
+
+# print(data.count(find))
+
+
+
+# condational statments
+
+# opeators:
+
+# 1) compersion operator
+
+#  -----> true | False
+
+    # 1) equal to  ==
+    # 2) not equal to   !=
+    # 3) >
+    # 4) <
+    # 5) <=
+    # 6) >=
+
+
+# print("10" == 10)
+
+# print(10 != 10)
+
+# print(10 > 4)
+
+# print(34 < 23)
+
+
+
+# if, elif, else
+
+# if condation:
+    # block of code
+
+
+# if 10 == 10:
+#     print("he;lllooo")
+#     print("okkkkkkkkkk")
+#     print("doneeeeeeeeee")
+
+
+# print("heyyyyyyyyyyyyyyyyyy!")
+
+
+
+# if 10 == 11:
+#     print("tehy are equal")
+
+# else:
+#     print("they are not eqaul")
+
+
+# Voatting system
+
+
+age = int(input("Enter your age : "))
+
+if age > 18:
+    print("you are elgiable for vaot")
+
+elif age == 18:
+    print("welocome this is your first vaot")
+
+elif age == 19:
+    print("this is your seoncd voat")
+
+else:
+    print("your are not eligible for vaot")
+
+
+8219836118
+
+
+
+
+
