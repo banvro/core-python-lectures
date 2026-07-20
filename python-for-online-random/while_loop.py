5ac58aa0d02d72fd81d6cc6594a57cb4ff17d044
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-20 17:50:48 +0530

    Create while_loop.py

---
+++ python-for-online-random/while_loop.py
@@ -0,0 +1,214 @@
+# Objects:
+#     1) ittrable objects
+#         string, list, tuple, set, dictionry
+    
+    
+#     2) non ittrable objects
+#         integer, float, boolean, complex
+        
+
+# len(), count(), for loop, indexing, slicing
+# ... ittrable objects
+
+
+# Loop:
+#     repetaion / Ittration
+
+
+# sequence based   |  range()
+
+
+# zx = "this is a car"
+
+# for q in zx:
+#     print(q)
+#     print("hellooo")
+#     print(q, q, q, q, q, q, q, q)
+#     print("doneeee")
+
+
+
+# x = 10020300203
+
+# x = str(x)
+
+# for i in x:
+#     print(int(i) + 5)
+    
+    
+    
+# q = 100020030020300000034000023000
+
+# q = str(q)
+
+# for i in q:
+#     if i != "0" and i != "1":
+#         # if i != "1":
+#             print(i)
+    
+    
+    
+# range(start, end, increment)
+    
+# for i in range(1, 10, 2):
+#     print(i)
+    
+    
+    
+# for i in range(15, 0, -1):
+#     print(i)
+    
+    
+    
+# start ---> 0
+# end ---> n - 1
+# increment ---> 1
+    
+    
+# for i in range(3, 10, 2):
+#     print(i)
+    
+    
+    
+# for u in range(10, 0, -1):
+#     print(u)
+    
+
+# while loop
+    # for loop ---> sequence based loop
+    
+    # while loop --> condation based loop
+
+
+# while condation:
+#     block of code....
+
+
+# while 10 == 10:
+#     print("hellooo")
+    
+
+# x = 1
+
+# while x < 11:
+#     print(x)
+    
+#     x = x + 1
+
+
+
+# -5
+# -4
+# -3
+# -2
+# -1
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+# x = -5
+
+# while x < 6:
+#     print(x)
+    
+#     x = x + 1
+
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+# 88888888
+# 999999999
+
+# print("hello" * 3)
+
+# x = 1
+
+# while x < 10:
+#     print(x * str(x))
+    
+#     x = x + 1
+
+# for i in range(1, 10):
+#     print(i * str(i))
+
+
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+#           *
+#         * *
+#       * * *
+#     * * * *
+#   * * * * *
+# * * * * * *
+
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# for i in range(5, 0, -1):
+#     print(i * "* ")
+
+# x = 5
+
+# while x > 0:
+#     print(x * "% ")
+#     x -= 1
+
+#           *
+#         * *
+#       * * *
+#     * * * *
+#   * * * * *
+# * * * * * *
+
+
+# for i in range(6, 0, -1):
+#     print("  " * (i-1) + (7 - i) * " *")
+
+
+    # 4
+for i in range(1, 8):
+                #   (1, 5)
+    for j in range(1, i+1):
+        
+        print(j, end = " ")
+    
+    print()
+
+
+# print("helllooooo", end = "  ")
+# print("kkkkkk", end = "--")
+# print("donneeeee")
+
+
+
+# print("hello this \nis a car and \nthis is a good \ncar")
+    
+    
+
+
+
+
+
+
+
+
+
+# 
