77c887d913bde015bdbe13a25078f1f0dccd1725
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-24 13:16:38 +0530

    Create basic_core_funciton.py

---
+++ python-for-online-random/basic_core_funciton.py
@@ -0,0 +1,135 @@
+# Variables
+# Dat types
+# print()
+# comments
+
+# function
+
+# type()
+#     ---> help to print the type of data a varibale hold
+
+
+# x = 10 + 20j
+
+# print(type(x))
+
+
+# input()
+#     ---> help to get input from user...
+
+
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+
+
+# name = input("Enter your name : ")
+
+# print("User enter there name : ", name)
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+# number = input("Enter your number : ")
+
+# print("User name is", name, "and user age is ", age, "and user phone number is", number)
+
+
+
+
+# number1 = input("Enter first number : " )
+# number2 = input("Eter second number : ")
+
+# zx = number1 + number2
+
+# print(zx)
+
+
+# input()
+#     ---> string
+
+# print("500" + "1000")
+
+# print(500 + 1000)
+
+
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter second number : "))
+
+# zx = num1 + num2
+
+# print(zx)
+
+
+# born_y = int(input("Enter your born year : "))
+
+# age = 2026 - born_y
+
+# print("User age is : ", age)
+
+
+# len()
+
+
+# Objects:
+#     1) Ittrable objects
+#         --> string, list, tuple, set, dictionry
+    
+#     2) Non Ittrable Objects
+#         --> integer, float, boolean, complex
+
+# -------------/
+# len(), count(), for loop, indexing, slicing...
+# Ittrable objects
+
+# len()
+#     ---> help to know about the total number of elements in an ittrable object
+
+
+# zx = "this is a car and thi s is a good car"
+
+# print(len(zx))
+
+
+# x = "5.0?*8"
+
+# print(len(x))
+
+# count()
+#     ---> help to know about the occurence of an element in an ittrbale object
+
+
+zx = "this is a car and this is a good car"
+
+q = zx.count("is")
+
+print(q)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
