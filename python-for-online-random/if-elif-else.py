2ac84a6ce16619deea61dcbdd5da08d89fa2b8ae
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-07-31 21:10:07 +0530

    Create if-elif-else.py

---
+++ python-for-online-random/if-elif-else.py
@@ -0,0 +1,79 @@
+
+# username = "Naresh"
+# password = "naresh@1234"
+
+# u_name = input("Enter username : ")
+# p = input("Enter password : ")
+
+# if u_name == username and p == password:
+#     print("Login Sucessfully..!")
+
+# else:
+#     print("Wrong.....!")
+
+
+
+# Voating system
+
+# age = int(input("Enter your age : "))
+
+# if age > 18:
+#     print("you are eligible for voat..!")
+
+# else:
+#     print("you are not eliigibel for vaot.")
+
+
+# marks = int(input("Enter percntage : "))
+
+# if marks > 34:
+#     print("pass")
+
+# else:
+#     print("fail")
+
+
+# Voating system
+
+# age = int(input("Enter your age : "))
+
+# if age > 18:
+#     print("you are elgibele for vaot.")
+
+# elif age == 18:
+#     print("welcome! this is yur first voat")
+
+# elif age == 19:
+#     print("this is yur second voat")
+
+# else:
+#     print("you are not eligible..!")
+
+
+
+# grading system
+
+
+pst = int(input("Enter percentage : "))
+
+if pst > 90:
+    print("A")
+
+elif pst > 80:
+    print("B")
+
+elif pst > 70:
+    print("C")
+
+elif pst > 60:
+    print("D")
+
+else:
+    print("Fail")
+
+
+
+
+
+
+
