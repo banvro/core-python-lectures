00e9af80b52f3f44f53f48c087b46ea576fd9a63
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-01 17:05:28 +0530

    Add files via upload

---
+++ core-python-for-dashboard/if-elif-else.py
@@ -0,0 +1,6 @@
+Class Board : https://miro.com/app/board/uXjVH2XsMnA=/?share_link_id=334509207726
+Notes : https://theagleye.com/n/python/if-elif-else-conditional-statements
+
+Class code : 
+    
+    
