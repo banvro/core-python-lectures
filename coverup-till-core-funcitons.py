3cc790c27a66a5a909e523241d2ea6827520a505
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-07 13:11:44 +0530

    Create coverup-till-core-funcitons.py

---
+++ coverup-till-core-funcitons.py
@@ -0,0 +1,342 @@
+
+# History
+# Varibaes
+# Keywords
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Data Types:
+#     1) Integer ---- int
+#     1, 2, 4, 100, -12, 34, 5, 12
+    
+#     10e307
+    
+#     2) Float ----> float
+#         10.1, 1.0, 5.9
+    
+#     3) String ---> str
+#         'this is a string'
+#         "this is also a strig"
+#         '''this is also a strng'''
+#         """this is alos a string"""
+        
+#         "10"
+    
+#     4) Boolean ---> bool
+#         True / False
+        
+#     5) Complex
+#         a + bj
+#         10 + 20j
+
+# -------------------------------
+    # 6) list
+    # 7) tuple
+    # 8) set
+    # 9) dictionry
+    
+
+# functions:
+#     1) Built in function
+    
+#     2) User
+    
+
+
+
+
+
+
+# print("hello world")
+
+# print('heyyyyyyyyyyyyy dsfsdbfb sdif sdfi sdf sd fdsif s fsd fdsif s fids fds ifdsf dsf s fds fdfsf fs')
+    
+
+
+# multi line string 
+
+# print("""
+# name : naresh
+# age ---- 25
+# number ------- 8219836118
+# asdkjasd
+# asdbasdkbjasd
+# askdbaskdbjas""")
+    
+
+
+# a = 10
+# b = 20
+# c = a + b
+
+# hello = "ok"
+
+# print("hello")
+    
+    
+   
+# a = 10
+# b = 20
+# c = a + b
+
+# print(a, b, c, "Hlleo", c, c, c, a, a, a)
+    
+# print(a)
+# print(b)
+# print(c)
+# print("hello")
+    
+    
+    
+# a = 12
+# b = 34
+# c = a + b
+
+
+# --> the sum of a(value) and b(value) is : c(value)
+
+# print("the sum of", a, "and", b, "is : ", c)
+
+
+# name = "Naresh"
+# age = 25
+# number = 8219836118
+# email = "naresh@gmail.com"
+
+# print("Your name is", name, "and your age is ", age, "and yuro phoe number is ", number, "and email id is ", email)
+
+# f-string (formatted string)
+
+# print(f"your name is {name} and you age is {age} and your phone number is {number} and your email id is {email}")
+
+
+
+# a = 10
+# b = 20
+# c = a * b
+
+# print(f"the multiplication of {a} and {b} is : {c}")
+
+# print("the multiplication of", a, "and", b, "is : ", c)
+
+
+
+
+a = 10
+
+b = 78
+
+c = b / a
+
+# sepration --> sep
+
+# print(a, b, c, "hello", sep = " ")
+
+# end
+
+
+
+
+
+
+a = 10
+
+b = 78
+
+c = b / a
+
+# end = "\n"
+# print(a, end = "   ")
+# print(b, end = "")
+# print(c)
+
+
+# 1 tab --> 4 spaes
+
+# print("this is \na car and \nthis is a \ngood car")
+
+
+# name = "naresh"
+
+# age = 24
+
+# number = "done"
+
+# print(name, age, sep = " - ", end = "::")
+
+# print(f"Phone number : {number}")
+
+
+
+# print()
+
+# type()
+# ---> help us to know the type of data a variable hold
+
+# a = 10 + 45j
+
+# print(type(a))
+
+# zx = 100
+
+# print(type(True))
+
+# x = "y"
+
+# y = "a"
+
+# zx = 34
+
+# x = zx
+
+# print(x)
+
+
+
+
+
+
+
+
+
+============================================== At 11:30 =====================================================================================
+
+
+
+# print()
+# type()
+
+# input()
+#     --> take input from user
+
+# a = 10
+
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+
+# print(f"user name is {name} and user age is {age}.")
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number " )
+
+# zx = num1 + num2
+
+# print(zx)
+
+
+
+# input() -----> string
+
+# print("100" + "300")
+# ---> concatnation : joining two strings
+
+# print(100 + 300)
+
+
+# Type casting:
+#     to chnage the data type of data.
+
+# intger ---> int()
+# float ---> float()
+# string --> str()
+# boolean ---> bool()
+
+# a = 100
+
+# print(a, type(a))
+
+# b = float(a)
+
+# print(b, type(b))
+
+
+# zx = "100"
+
+# print(zx, type(zx))
+
+# x = int(zx)
+
+# print(x, type(x))
+
+
+
+# num1 = int(input("Enter 1st number : "))
+# num2 = int(input("Enter 2nd number : "))
+
+# zx = num1 + num2
+
+# print(zx)
+
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# zx = int(num1) + int(num2)
+
+# print(zx)
+
+
+# --------------------------
+
+# Write a program, get the born year from user and calculat the age.
+
+
+
+# objects ---> data type
+
+# 1) ittrable objets
+#     ---> string, list, tuple, set, dict
+
+# 2) non ittrable
+#     --> integer, flaot, boolean, complex
+
+# -------> 
+# len(), count(), for loop, indexing, slicng..... ittrable objets
+
+
+
+
+# len()
+#     --> help us to know total number of elements in an ittrable object
+
+
+# zx = "this is"
+
+# print(len(zx))
+
+
+# zx = 12030
+
+# x = str(zx)
+
+# print(len(x))
+
+# -------------------------------------
+
+# count()
+#     --> help us to know about the occurence of an element in a ittrable object.
+
+# vaiable_name.count(?)
+
+
+x = "hello, how are you, how how ok"
+
+y = x.count(" ")
+
+print(y)
+
+
+
+
+zx = 100002000010010
+
+x = str(zx)
+
+qw = x.count("0")
+
+print(qw)
