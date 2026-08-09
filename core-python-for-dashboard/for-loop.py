2fe287e8676d9b78cb9a74aee68d1cfee73c3c6f
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-09 18:19:13 +0530

    Create for-loop.py

---
+++ core-python-for-dashboard/for-loop.py
@@ -0,0 +1,90 @@
+Notes : https://miro.com/app/board/uXjVHzsOU-w=/
+
+-------------------------------------------------------------------------------------------------------
+data = "this is"
+
+for x in data:
+    print(x)
+    print("hellooo")
+    print(x, x, x, "heyyy")
+    print("donneee....")
+
+
+xyz = "12345"
+
+for i in xyz:
+    print("helloooo....!", i)
+
+
+
+yz = "1003"
+
+for i in yz:
+    print(i)
+    print("heyyyyyyyyyyyyyyyy")
+
+
+for ---> string, list, tuple, set, dicitonry
+
+
+not able to run for loop
+integer, float, boolean, complex
+
+
+
+
+zx = [12, 34, 4, 67, 89, 34, "heloo"]
+
+for i in zx:
+    print(i)
+    print("oooooooooooooooo")
+
+print("nice..................")
+
+
+
+zx = 1002030203002030230
+
+pq = str(zx)
+
+for i in pq:
+    if i == "0":
+        print("helloooo")
+    else:
+        print(i)
+
+
+
+
+q = [1, 45, 23, 34, 2, 12, 23, 45, 56, 223]
+
+
+for o in q:
+    if o % 2 == 0:
+        print(o)
+
+
+for x in range(11, 40, 1):
+    print(x)
+
+
+for i in range(15):
+    print(i)
+
+for i in range(3, 15):
+    print(i)
+
+
+
+for i in range(3, 20, 2):
+    print(i)
+
+
+
+# 20 --- 5
+
+for i in range(20, 4, -2):
+    print(i)
+
+
+
