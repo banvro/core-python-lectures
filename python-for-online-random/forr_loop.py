cc18ec3b5ecf9cb1dd11e55e29157007b6989a28
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-11 13:34:01 +0530

    Create forr_loop.py

---
+++ python-for-online-random/forr_loop.py
@@ -0,0 +1,106 @@
+
+
+
+# for q in range(5, 15, 1):
+#     print(q)
+#     print("helloooooo")
+#     print("ok")
+#     print(q, q, q, q)
+#     print("oooooooooooooooooo")
+
+
+# for i in range(1, 11, 1):
+#     print(i)
+
+
+
+# for i in range(4, 23, 5):
+#     print(i)
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# ,
+# ,
+# ,
+# 2 x 10 = 20
+
+# for i in range(1, 51):
+#     print("2 x", i, "=", 2*i)
+    
+    
+# 100 x 1 = 5
+# 99 x 2 = 10
+# 98 x 3 = 15
+# 97 x 4 = 20
+# 96 x 5 = 25
+# 95 x 6 = 30
+# 94 x 7 = 35
+# 93 x 8 = 40
+# 92 x 9 = 45
+# 91 x 10 = 50
+
+# for i in range(0, 10):
+#     print(100 - i, "x", i + 1, "=", 5*(i+1))
+    
+    
+    
+# 1
+# 2 2
+# 3 3 3 
+# 4 4 4 4
+# 5 5 5 5 5
+# 6 6 6 6 6 6
+# 7 7 7 7 7 7 7
+# 8 8 8 8 8 8 8 8
+# 9 9 9 9 9 9 9 9 9 
+
+
+# print("2 " * 5)
+    
+
+
+# for i in range(1, 10):
+#     print(i * (str(i) + " "))
+
+
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3 
+# 2 2 
+# 1
+# 2 2
+# 3 3 3 
+# 4 4 4 4
+# 5 5 5 5 5
+
+
+# for i in range(5, 0, -1):
+#     print(i * (str(i) + " "))
+
+
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3 
+# 2 2 
+# 1
+# 2 2
+# 3 3 3 
+# 4 4 4 4
+# 5 5 5 5 5
+
+for i in range(5, -4, -1):
+    if i > 0:
+        print(i * str(i))
+    else:
+        print((abs(i) + 2)  * str(abs(i) + 2))
+
+x = -10
+
+print(abs(x))
