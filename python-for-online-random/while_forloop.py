163be3903a12ef5355a1b0b5d789ef79d7476b97
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-12 13:50:12 +0530

    Create while_forloop.py with loop examples
    Added a Python script demonstrating various loop constructs and string manipulations.

---
+++ python-for-online-random/while_forloop.py
@@ -0,0 +1,205 @@
+
+
+
+# for i in range(50, 11, -1):
+#     print(i)
+
+# Ittrable data:
+#     string, list, tuple, set dictionry
+
+
+# zx = "hello how"
+
+
+# for i in zx:
+#     print(i)
+#     print("heyyyyyyyyyyy")
+#     print("okkkkk", i, i, i, i, i)
+#     print("done")
+#     print("nice........!")
+
+# print("ooooooooooooooo")
+
+
+# zx = 100002000201103000
+
+# 1 ---> hey
+# 0 ---> Nice
+# 2 ---> Ok
+# 3 --- done
+
+
+# zx = 100002000201103000
+
+# zx = str(zx)
+
+# for i in zx:
+#     if i == "1":
+#         print("hey")
+#     elif i == "0":
+#         print("Nice")
+#     elif i == "2":
+#         print("ok")
+#     else:
+#         print("Done")
+
+
+
+# x = 321
+
+# zx = str(x)
+
+# for i in zx:
+#     print(int(i) + 2)
+
+
+# while loop
+# ----> condation based loop
+
+# for loop
+#     ----> data
+#     ---> range()
+
+
+# while condation:
+    # block of code....
+
+
+
+# while 10 == 10:
+#     print("heloooooo")
+#     print("hiiiiiiiiiiiii")
+
+
+
+# a = 1
+# while a < 11:
+#     print(a)
+#     a = a + 1
+
+# -----------------
+# -5
+# -4
+# -3
+# -2
+# -1
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+# zx = "nice day"
+
+# print(zx[7])
+
+# print(zx[-3])
+
+# x = "this is car and nice car"
+
+# print(x[8 : 20])
+
+# print(x[ : : -1])
+
+# print(x[-8 : -4])
+
+
+# x = "this is car and nice car"
+
+# a = 0
+
+# while a < len(x):
+
+#     print(x[a])
+
+#     a = a + 1
+
+
+# for i in range(1, 11):
+#     print(i)
+#     break
+
+
+
+# zx = 10312301927320137
+
+# for i in str(zx):
+#     if i == "7":
+#         break
+#     print(i)
+
+
+# continue
+
+
+# for i in range(1, 11):
+#     if i == 5:
+#         continue
+
+#     print(i)
+
+
+
+
+# zx = 103102001020030023020300002023000300000
+
+# for i in str(zx):
+#     if i == "0":
+#         continue
+
+#     print(i)
+
+
+
+
+# pass---
+
+
+# for i in range(1, 11):
+#     pass
+
+# -------------------------------------
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+
+# for i in range(10, 0, -1):
+#     print(i * "* ")
+
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+a = 5
+
+while a > -4:
+    
+    if a > 0:
+        print((6 - a) * "* ")
+    else:
+        print((4 - abs(a)) * "* ")
+        # print(a)
+
+
+    a = a - 1
+
+8219836118
+
+
+
+
+
