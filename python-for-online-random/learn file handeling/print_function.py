ca35b6b56a20f139a45b717ebe22f3ab7d7efad3
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-18 11:16:48 +0530

    Refactor print_function.py for clarity and formatting

---
+++ python-for-online-random/learn file handeling/print_function.py
@@ -0,0 +1,178 @@
+
+10.4
+
+"10.5"
+
+'heyyyyy'
+
+"this is also a string"
+
+'''this is also a string'''
+
+"""this is also a string"""
+
+
+
+print(11 == 10)
+
+
+print("heloooo ho are you")
+
+print('heyyyyyyyyy')
+
+---> use only printing single line
+
+
+
+print('''this is working''')
+
+print("""hyyyyyyyyyyyyyy""")
+
+
+
+
+# print('this is a car and this is agood car how rare yo akjbdkas da dad a kd asd as da das d askd asd sak 
+# dkas dad akd askd')
+
+
+print('''
+
+helooo
+
+how are you
+
+
+done
+''')
+
+
+''
+"" ----> single line string
+
+
+'''''' | """""" -> this is multi line string
+
+
+
+print("hey how are you")
+
+
+a = 10
+
+b = 20
+
+c = a + b
+
+print(c)
+
+
+print("Sorabh singh")
+
+
+a = 10
+
+b = 20
+
+c = a + b
+
+print(a, b, "hello", c, c, c, c, "Nice")
+
+----->
+
+The sum of a(value) and b(value) is : c(value)
+
+the sum of 10 and 20 is : 30
+
+a = 4
+
+b = 6
+
+c = a + b
+
+print("the sum of", a, "and", b, "is :", c)
+
+
+name = "Naresh"
+age = 25
+numbe = 8219836118
+mail = "programography@gmail.com"
+
+print("my name is", name, "and my age is", age, "and my phone number is", numbe, "and my email id is", mail)
+
+
+F-string (formatted string)
+
+print(f"My name is {name} and my age is {age} and my phone numb er is {numbe} and my email id is {mail}")
+
+
+
+a = 10
+
+b = 20
+
+c = a + b
+
+print(f"the sum of {a} and {b} is : {c}")
+
+8219836118
+
+# name = "Naresh"
+# age = 25
+# numbe = 8219836118
+# mail = "programography@gmail.com"
+
+
+# print(f"""
+# my name is ------> {name}
+
+# age ------> {age}""")
+
+
+
+# a = 10
+
+# b = 20
+
+# c = 30
+
+# y = 90
+
+# # sep = ? seprater
+
+# print(a, b, c, y, a + b, "hello", y - b, sep = "")
+
+
+
+
+
+# print()
+
+
+
+# help(print)
+
+# \n
+
+# print("heloooooooooo \nhow are you \nwhat are your \ndoing nice")
+
+# 1 tab = 4 spaces
+
+# print("nice ctar nice")
+
+
+# help(print)
+
+print("hell\noooo\n", end = " ")
+
+print("niceee", end = " ")
+
+print("ok")
+
+
+
+
+
+
+
+
+
