b9e39a35daf2fde0684a4fd7639665d243cb0737
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-19 10:47:15 +0530

    Create input_.py

---
+++ python-for-online-random/learn file handeling/input_.py
@@ -0,0 +1,168 @@
+# core fuctions
+
+# print() 
+#     ----->  to see the output  in terminal.
+
+
+# type()
+#     ---> help us to know about the type of data we have.
+
+
+# x = {12, 45, 56, 67, "helooo", True}
+
+# tuple
+
+# print(type(x))
+
+# [] ---> list
+# () ----> tuple
+# {} ----> set
+# {key : value, key : value}  ---> dictionry
+
+
+# zx = 91.0
+
+# print(type(zx))
+
+# -----------------------------------------
+
+# input()
+#     ----> help to get input from user.
+
+
+
+
+# x = 10
+
+# y = 20
+
+# z = x + y
+
+# print(z)
+
+
+# name = input("Enter your name : ")
+
+# print("User name is : ", name)
+
+# name = input("Enter your name : ")
+# "Naresh"
+
+# age = input("Enter your age : ")
+# "25"
+
+# print(f"user name is {name} and user  age is {age} ")
+
+
+# input() ---> input always return a string data 
+
+
+# wap to take input from user and add the numbers
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# zx = num1 + num2
+
+# print(f"The sum of {num1} and {num2} is : {zx}")
+
+# print("10000" + "23000")
+
+# print(10000 + 23000)
+
+# Concatenation : Joining two string
+
+
+# print("hi" + " " + "Nice" + "344" + 4)
+
+
+# "1000" -----> 1000
+
+
+
+# Type casting / Type Conversion    
+    # : to chnage the type of data from one to another
+
+# integr -----> int()
+# float ------> float()
+# string ---> str()
+# boolean ----> bool()
+
+# zx = 10
+
+# print(zx, type(zx))
+
+# q = float(zx)
+
+# print(q, type(q))
+
+
+
+
+# zx = "1290"
+
+# print(zx, type(zx))
+
+# x = int(zx)
+
+# print(x, type(x))
+
+# x = 0
+
+# q = bool(x)
+
+# print(q)
+
+
+# print(True + True + True * False)
+
+# print(bool(-1))
+print()
+
+
+
+
+
+# num1 = int(input("Enter first number : "))
+
+
+# print(num1, type(num1))
+
+
+# num1 = float(input("Enter first number : "))
+
+# num2 = float(input('Enter second number : '))
+
+# zx = num1 + num2 
+
+# print(f"the sum of {num1} and {num2} is : {zx}")
+# float
+
+
+
+# input()
+# concatenation
+# Type casting
+
+
+
+# take a birth year from user and calcuate the age and print in proepr fromat 
+
+# --> year = 2003
+
+# --> you are 23 years old
+
+
+birth = int(input("Enter your birth year : "))
+
+age = 2026 - birth
+
+print(f"You are {age} years old.")
+
+
+
+
+
+
+
+print()
