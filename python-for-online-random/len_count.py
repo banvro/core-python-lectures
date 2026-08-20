e90acf63b85ef3821e0083fb9f5282155c770873
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-20 10:47:11 +0530

    Create len_count.py

---
+++ python-for-online-random/len_count.py
@@ -0,0 +1,98 @@
+
+
+
+# print()
+# input()
+# type()
+
+# ------------------
+
+# len()
+# count()
+
+# Python ----> Objects
+#         # ------> data types
+
+# Objects are deividee into  two cat...
+
+# 1) Ittrable objects
+#     -> string, list, tuple, set, dictionry
+
+# 2) Non-ittrable objects
+#     -> integer, float, boolean, complex
+
+# # -----------------
+# len(), count(), indexing, slicing, forloop, ------> works only with ittrable objects
+
+
+
+# len()
+#     ----> help to know abut the total number  of elements in an ittrable object.
+
+
+# zx = "this is a car a nd this is a good car"
+
+# print(len(zx))
+
+
+
+
+# x = "12322"
+
+# x = str(x)
+
+# print(len(x))
+
+
+# zx = [12, 34, "hi", "Nice", True]
+
+# print(len(zx))
+
+
+# ------------------------------------------
+
+# wap to take input from user and print the total number of elements user pass. print in proepr way. 
+
+# the summery is:::::
+# Your data : ____________
+# total elements : _________
+
+# inpp = input("Enter data : ")
+
+# l = len(inpp)
+
+# print(f'''
+# :::::: the summery is ::::::
+# Your data ---> {inpp}
+# totla elements ----> {l}
+# ''')
+
+
+
+# ------------------------------------------
+
+
+# count()
+#     ::: help to know about the occurence of an element in an ittrable object.
+
+
+# syntax
+
+# data.count(?)
+
+# zx = "how  are you, what you doin nice you"
+
+# q = zx.count("o")
+
+# print(q)
+
+
+x = 1000002030400004003040340340
+
+x = str(x)
+
+w = x.count("0")
+
+print(w)
+
+
