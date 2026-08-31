d811b69136ea5ebfe1aa8efa287facd945cfabcb
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-08-31 12:26:07 +0530

    Add for_lopppp.py with conditional statements and loops

---
+++ python-for-online-random/for_lopppp.py
@@ -0,0 +1,138 @@
+# Condational statments:
+#     nested condatitional statments
+
+# Voating system:
+#     18 > 
+#         100 >   
+#             ----> need to verify
+
+# age = int(input("Enter your age : "))
+
+# if age > 18:
+
+#     if age > 100:
+#         print("need to verify")
+    
+#     elif age == 19:
+#         print("you are 19")
+    
+#     else:
+#         print("eligible for vaot")
+
+# else:
+#     print("no eligible for vaot")
+
+
+# ________________________________________________________________________________________________
+
+
+# username_c = "root"
+# password_c = "Root@123"
+
+
+
+# username = ?
+
+
+# check username --> 
+#     password = ?
+#         ---> login sucessfully!
+
+# ---> incorrect username
+
+
+# Loops
+    # ---> 
+    #     for loop
+    #     while loop
+
+
+# for loop ----> with range function
+
+
+
+# range()
+    # ----> help to genrate data
+
+
+
+# range(start, end, increment)
+
+# # 10 --- 30
+
+# range(10, 31, 1)
+
+
+
+# ::::: for loop :::::
+
+# syntax
+
+# for variba_name in range(st, end, incremnt):
+#     block of code.....
+
+
+# for i in range(50, 101, 5):
+#     print(float(i))
+
+
+# ----------------------------------------------
+
+
+# for x in range(1, 11):
+#     print("helloooooooooo")
+
+# ----> ittration
+
+# for i in range(3, 14, 1):
+#     print(i)
+#     print("nice......")
+#     print(i, i, i, i, i, i, i)
+#     print("doneeeeeeee")
+#     print("ok")
+
+# print("heyyyyyyyyyyyyyyyyyyy")
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# .
+# .
+# .
+# .
+
+# 2 x 10 = 20
+
+
+for i in range(10, 1, -1):
+    print(i)
+
+
+1------50
+
+3 % ---> fizz
+5 % ----> buzz
+
+3 and 5 % --> fizzbuzz
+
+
+1
+2
+fizz
+4
+# buzz
+# fizz
+
+
+
+
+
+
+
+
+
+
+
+
+
