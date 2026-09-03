275fecb3933194d4d8845eab0314775e9b35c9ba
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-09-03 10:38:19 +0530

    Create control statments.py

---
+++ python-for-online-random/control statments.py
@@ -0,0 +1,128 @@
+# Control statments:
+#     1) break
+#     2) continue
+#     3) pass
+
+# ----> use to control the loops
+
+
+# break : -> when we need to break the loop
+
+
+# for i in range(1, 11):
+#     print(i)
+#     break
+
+
+# for i in range(1, 16):
+#     print(i)
+
+#     if i == 5:
+#         break
+
+
+# zx = "this is a car"
+
+# for i in zx:
+#     if i == "a":
+#         break 
+#     print(i)
+
+# ________________________________
+
+# 2) continue
+#     --> skip the statments
+
+
+# for i in range(1, 20):
+#     print("heloooo")
+#     continue
+#     print(i)
+
+
+# for i in range(1, 11):
+#     if i == 5:
+#         continue
+
+#     print(i)
+
+# zx = "this is a car and train nice car bus"
+
+# for i in zx:
+#     if i == " " or i == "a" or i == "t":
+#         continue
+#     print(i)
+
+# ______________________________
+
+# zx = 1000102300040005006000020300120
+
+# 1
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 3
+# 1
+# 2
+
+# zx = 1000102300040005006000020300120
+
+# qw = str(zx)
+
+# q = ""
+
+# for i in qw:
+#     if i == "0":
+#         continue
+#     q = q + i #11
+
+# print(q, end = "")
+
+
+# print("2" + "2")
+
+# zx = 1000102300040005006000020300120
+
+# qw = str(zx)
+
+
+# for i in qw:
+#     if i == "0":
+#         continue
+#     print(i, end = "-")
+# _________________________________________
+
+
+# 3) pass
+#     ---> do nothing, it just indecate for now leave this will add code leter
+
+# for i in range(1, 11):
+#     pass
+
+
+# print("heyyyyy")
+
+
+# if 10 == 10:
+#     pass
+
+# _________________________________________
+
+qw = "nice car i wana buy this car"
+
+
+# exclude ---> " ", "c","a" 
+
+nieriwnbuy
+
+
+
+
+
+
+
+
