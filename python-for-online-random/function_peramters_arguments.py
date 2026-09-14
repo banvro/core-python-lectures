d40dd9541c36639a13bb131c676f343254ddf859
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-09-14 10:54:41 +0530

    Create function_peramters_arguments.py

---
+++ python-for-online-random/function_peramters_arguments.py
@@ -0,0 +1,100 @@
+# functions:
+#     ---> user define function
+
+
+# syntax
+
+
+# def function_name():
+#     # function code...
+
+
+
+
+
+
+
+# how to declear a function
+
+# def xyz():
+#     print("helloooooo")
+#     print("niceeeeeeeeeeeee")
+#     print("okkk")
+#     print("done..!!!")
+
+
+# # calling a function
+
+# xyz()
+
+# xyz()
+
+
+# print("oooooooooooooooooooooooooooo")
+
+# a = 10
+
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+# xyz()
+
+
+# -----> help us for code optimization
+
+
+# Perameters & arguments
+
+# def xyz():
+#     print("good moring! Ramesh")
+#     print("how are you..")
+
+
+# xyz()
+
+# xyz()
+
+# Parameters : are the variables we put in breckets at the function declaration time
+
+# def helo(a):
+#     print("Good mornning", a)
+#     print("how are you...")
+
+# arguments ---> are the values we pass at calling time
+
+# helo("Mohan")
+
+# helo("Moris")
+
+
+# def xyzz(a, b):
+#     zx = a + b
+#     print(f"The sum of {a} and {b} is : {zx}")
+
+
+# xyzz(10, 45)
+
+
+# xyzz(1, 2)
+
+
+
+table(2)
+
+
+2 x 1 = 2
+2 x 2 = 4
+.
+.
+.
+..
+
+
+
+
+
+
+
