01978a5756276da1d9f1222bfff22b859af11b37
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-09-15 12:18:33 +0530

    Create bhasker_core_functions.py

---
+++ python-for-online-random/bhasker_core_functions.py
@@ -0,0 +1,141 @@
+
+# a = 10
+
+# b = 20
+
+# c = 30
+
+# x = a + b + c
+
+# print(a, b, "hello", c, x, x, "heyyy", sep = " ")
+
+# a = 10
+# b = 20
+
+# x = a + b
+
+
+# output : the sum of 10 and 20 is : 30
+
+
+# a = 10
+# b = 20
+# c = 30
+
+# x = a + b + c
+
+# print("the sum of", a, "and", b, "and", c, "is : ", x)
+
+# a = 10
+# b = 20
+# c = 30
+# f-string : 
+
+# x = a + b + c
+
+# print(f"the sum of {a} and {b} and {c} is : {x}")
+
+# input():
+#   ---> help to get input from user
+
+
+
+# x = 5
+# y = 6
+
+# z = x + y
+
+# print(z)
+
+
+
+# name = input("Enter your name : ")
+
+# print("user name is : ", name)
+
+# num1 = ?
+# num2 = ?
+
+
+# input() ----> string
+
+# print("2" + "2")
+  # ----> concatenation : joining two string
+
+# print(2 + 2)
+
+# Type casting / Type conversion:
+#     convert the data type of data from one to another
+
+# integer ---> int()
+# float -----> float()
+# string ----> str()
+# boolean ---> bool()
+
+# zx = 10
+
+# p = float(zx)
+
+# print(zx, p)
+
+
+# num1 = int(input("Enter first number : "))
+
+# num2 = int(input("Enter second number : "))
+
+# zx = int(num1) + int(num2)
+
+# zx = num1 + num2
+
+# print(zx)
+
+# print()
+# input()
+
+# objects
+
+#   1) Ittrable objects
+#     --> string, list, tuple, set, dictriony
+
+#   2) Non ittrable objects
+#     --> integer, float, boolean, complex
+
+
+# len() count()
+# ----> ittrable objects
+
+
+# len()
+#   ---> help to know the total number of elements in an ittrable object.
+
+# zx = "th is is a car"
+
+# print(len(zx))
+
+
+
+
+# pq = 1002
+
+# print(len(pq))
+
+# ______________________________
+
+# count()
+#   ---> help to know abuut the occurence of an element in an ittrable object
+
+zx = "this is a car and this is a good car car color red car"
+
+# count() syntax
+
+# veriable_name.count("?")
+
+
+x = zx.count("car")
+
+print(x)
+
+
+8219836118
+
+
