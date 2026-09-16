8d5250b43b994ad5e7c1be1a58eb2a9add896f18
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-09-16 21:09:36 +0530

    Create veriables__.py

---
+++ veriables__.py
@@ -0,0 +1,113 @@
+
+
+
+
+
+# # ~!#@!$@%$^&^%&$)(*&^%#$@#!@#$)
+
+# # my@ = 10
+
+# # for = 90
+
+
+# import keyword
+
+# print(keyword.kwlist)
+
+# age = 90
+
+# Age = 45
+
+# aGe = 10
+
+# AGE = 9
+
+# print(aGe)
+
+
+# zx = "hello"
+
+# q = z
+
+# print(q)
+
+
+# _________________________________________
+
+# print("hello world")
+
+# print('heyyyyyyyyy')
+
+
+# zx = 10
+
+# pq = 3
+
+# d = zx * pq
+
+# print(d)
+
+
+
+# a = 10
+
+# b = 20
+
+# c = 20
+
+# z = a + b + c
+
+# print(a, b, "hello", c, z, "nice", c, c)
+
+
+
+
+# a = 10
+
+# b = 20
+
+# x = a + b
+
+# ---> the sum of 10 and 20 is : 30
+
+# print("the sum of", a, "and", b, "is : ", x)
+
+
+# solution -> f-string
+
+# print(f"the sum of {a} and {b} is : {x}")
+
+
+# name = "akjdbask"
+# age = 34
+# number = 2839462394
+# email = "adbasdja@gmk"
+
+
+# a = 3
+# b = 4
+
+# c = a + b
+
+# print(c)
+
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+
+# print("user name is : ", name, "and age is :  ", age)
+
+
+# # input() ---> str
+
+# "100"
+
+print("10" + "20")
+
+print(10 + 20)
+
+
+
+
+
