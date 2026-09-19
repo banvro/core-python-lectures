9421fd377d2bd22f392cf0a11f96781f01dbd743
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2026-09-19 15:22:13 +0530

    Create core_functions_done.py

---
+++ core_functions_done.py
@@ -0,0 +1,131 @@
+
+# Type casting : 
+#     help to convert the data type of data, from one to another.
+
+# integer --------> int()
+# float ----------> float()
+# string --------> str()
+# boolean ------> bool()
+
+
+# x = 10
+
+# y = float(x)
+
+# print(x)
+# print(y)
+
+
+# x = "100"
+
+# y = int(x)
+
+# print(y)
+
+# print("100" + "200")
+    # --> concatenation : joining two strings
+
+# print(100 + 200)
+
+# input() -----> string
+# "1" + "3"
+
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+
+# x = int(num1)
+# y = int(num2)
+
+# zx = x + y
+
+# print(zx)
+
+
+# Write a program where take input as user name, and user born_year and print messgae:
+
+# Your name is ____ and you are __ years old.
+
+# name = input("Enter your name : ")
+# born_year = input("Enter your born year : ")
+
+# b_year = int(born_year)
+
+# age = 2026 - b_year
+
+# print(f"Your name is {name} and age is {age}.")
+
+
+# print()
+# input()
+
+
+# len()
+#   ---> help to know about the total number of elements in an ittrable objects
+
+
+# zx = "th is is a car"
+
+# print(len(zx))
+
+
+# x = 1002
+
+# x = str(x)
+
+# print(len(x))
+
+
+# _____________________
+
+# count():
+#   ---> help us to know about the occurence of an element in an ittable object
+
+
+# syntax:
+
+# veriable_name.count("___")
+
+
+# zx = "this is a car and this is good car my car color is red car."
+
+# w = zx.count(" ")
+
+# print(w)
+
+
+# write a program to take input from user and 
+
+
+# zx = 1003000300300020200203
+
+# w = str(zx)
+
+# print(w.count(0))
+
+
+# _ __________________
+
+# Indexing slicing:
+
+# Indexing : when we need element a single element
+
+# zx = "Hoello"
+
+
+# print(zx[4])
+
+# print(zx.index("o"))
+
+# x = "this is a car"
+
+# print(x[8])
+
+
+q = "hello world"
+
+print(q[8])
+
+print(q[-3])
+
+
+
